5322c7e8c9286e1274fdc732ed1f755f32e39d81
SDH1001 <greedysdh@gmail.com>
2023-06-17 17:06:05 +0900

    Initial commit

---
+++ pybo/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default='', related_name='author_question')
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')  # 추천인 추가
+    view_count = models.IntegerField(default=0)  # 조회수 필드 추가
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='images/', null=True, blank=True)  # 이미지 필드 추가
+
+    def __str__(self):
+        return self.subject
+
+
+class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
+
+class QuestionCount(models.Model):
+    ip = models.CharField(max_length=30)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return self.ip
---
+++ pybo/views/base_views.py
@@ -0,0 +1,52 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+
+from ..models import Question, QuestionCount
+
+
+def get_client_ip(request):  # Add this function
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def index(request):
+    page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
+    question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    return render(request, 'pybo/question_list.html', context)
+
+def main(request):
+    return render(request, 'pybo/mainpage.html')
+
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    ip = get_client_ip(request)
+    #cnt = QuestionCount.objects.filter(ip=ip, question=question).count()
+    #if cnt == 0:
+       # qc = QuestionCount(ip=ip, question=question)
+        #qc.save()
+        #if question.view_count:
+        #    question.view_count += 1
+        #else:
+           # question.view_count = 1
+       # question.save()
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
